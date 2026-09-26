8b5efc3d86ac2a67b7d5264ba87ad05198978505
DxDad <159210393+DxDad@users.noreply.github.com>
2026-09-26 14:23:53 -0300

    Adicionar chaves do Google ADS no site

--- assets/js/main.js
+++ assets/js/main.js
@@ -336,6 +336,14 @@
   window.gtag('js', new Date());
   window.gtag('config', measurementId, { send_page_view: true });
 
+  function gtag_report_conversion() {
+   window.gtag('event', 'conversion', {
+    'send_to': 'AW-18476493707/KIshCO6My4YdEIvXo-pE',
+    'value': 1.0,
+    'currency': 'BRL'
+   });
+  }
+
   const gaScript = document.createElement('script');
   gaScript.async = true;
   gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
@@ -477,6 +485,7 @@
     whatsappLink.rel = 'noopener noreferrer';
     whatsappLink.title = 'Iniciar conversa pelo WhatsApp';
     whatsappLink.dataset.trackEvent = 'whatsapp_floating_click';
+    whatsappLink.dataset.googleAdsConversion = 'true';
     whatsappLink.innerHTML = `
       <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24">
         <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.009-.371-.011-.57-.011-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.876 1.213 3.074.149.198 2.096 3.2 5.076 4.487.709.306 1.262.489 1.694.626.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 0 1-5.031-1.378l-.361-.214-3.741.981.998-3.648-.235-.374a9.86 9.86 0 0 1-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 0 1 2.897 6.994c-.003 5.45-4.437 9.884-9.884 9.884m8.413-18.297A11.815 11.815 0 0 0 12.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 0 0 5.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 0 0-3.48-8.413Z"></path>
@@ -486,6 +495,12 @@
   }
 
   document.querySelectorAll('[data-track-event]').forEach((element) => {
-    element.addEventListener('click', () => window.gtag('event', element.dataset.trackEvent));
+  element.addEventListener('click', () => {
+    window.gtag('event', element.dataset.trackEvent);
+
+    if (element.dataset.googleAdsConversion === 'true') {
+      gtag_report_conversion();
+    }
   });
+});
 })();
